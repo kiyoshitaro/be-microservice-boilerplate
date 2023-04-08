f69c617ebd7273167e9b88c98f6d3b41d05244b8
Nguyễn Tuấn Hưng <hunglhp1998@gmail.com>
2023-04-08 09:45:34 +0700

    Update include.dto.ts ttl cache

--- libs/shared/src/dtos/include.dto.ts
+++ libs/shared/src/dtos/include.dto.ts
@@ -1,7 +1,11 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString, IsNumber } from 'class-validator';
 
 export class IncludeDto {
   @IsString()
   @IsOptional()
   include: string;
+  
+  @IsNumber()
+  @IsOptional()
+  ttl?: number;
 }
