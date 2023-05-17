8b4ec25d4897d98478483395916d184e0d6977f9
kiyoshitaro <hunglhp1998@gmail.com>
2023-05-17 21:33:59 +0700

    DELETE game-user-gateway

--- apps/game-user-gateway/src/validators/game-user.validate.ts
+++
@@ -1,8 +0,0 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
-
-@Injectable()
-export class GameUserValidationPipe implements PipeTransform<any> {
-  async transform(value: any, { metatype }: ArgumentMetadata) {
-    return value;
-  }
-}
--- libs/shared/src/configs/service-configs.ts
+++ libs/shared/src/configs/service-configs.ts
@@ -19,22 +19,7 @@ export class ConfigAppService {
       },
       transport: Transport.TCP,
     };
-    this.envConfig.gameUserGatewayPort = process.env.GAME_USER_GATEWAY_PORT;
-    this.envConfig.gameGatewayPort = process.env.GAME_GATEWAY_PORT;
-    this.envConfig.gameService = {
-      options: {
-        port: process.env.GAME_SERVICE_PORT,
-        host: process.env.GAME_SERVICE_HOST,
-      },
-    };
     this.envConfig.platformGatewayPort = process.env.PLATFORM_GATEWAY_PORT;
-    // this.envConfig.orchestratorService = {
-    //   options: {
-    //     port: process.env.ORCHESTRATOR_SERVICE_PORT,
-    //     host: process.env.ORCHESTRATOR_SERVICE_HOST,
-    //   },
-    //   transport: Transport.TCP,
-    // };
     this.envConfig.userService = {
       options: {
         port: process.env.USER_SERVICE_PORT,
@@ -42,13 +27,6 @@ export class ConfigAppService {
       },
       transport: Transport.TCP,
     };
-    // this.envConfig.templateService = {
-    //   options: {
-    //     port: process.env.TEMPLATE_SERVICE_PORT,
-    //     host: process.env.TEMPLATE_SERVICE_HOST,
-    //   },
-    //   transport: Transport.TCP,
-    // };
   }
 
   get(key: string): any {
